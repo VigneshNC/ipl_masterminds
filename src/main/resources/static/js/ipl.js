0431079749b585134d713ccea535143d12e49f30
Vignesh NC <Vignesh NC@VigneshNC>
2021-05-10 21:27:58 +0530

    location href fix

--- src/main/resources/static/js/ipl.js
+++ src/main/resources/static/js/ipl.js
@@ -153,7 +153,7 @@ $(document).ready(function() {
 		$("#navItemLogout, #navItemRules, #navItemUsers, #navItemPlayers, #navItemPointsTable").show();
 		$("#navItemLogin, #navItemRegister, #navItemRequestors, #divImport, #btnDelAllPlayers, .spanEditPlayer, #line, .hideField").hide();
 	} else {
-		if (location.href != "/" || location.href != "/ipl" || location.href != "/ipl/logout") {
+		if (window.location.pathname != "/" || window.location.pathname != "/ipl" || window.location.pathname != "/ipl/logout") {
 			location.href = "/";
 		}
 		$("#navItemLogin").show();
